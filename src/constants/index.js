e7a1290a1cecb1da73193056d232250ec22892b8
Phạm Ngọc <phamngoc@MacBook-Air-cua-Pham.local>
2026-04-09 17:29:03 +0700

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,365 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Zio Hair",
+    description:
+      "The site showcases services, stylist profiles, promotional offers, and facilitates easy booking inquiries.",
+    subDescription: [
+      "Developed UI from figma file, responsive, animation with Gsap.",
+      "Interact with the API to process data.",
+      "Designed responsive UI components with Tailwind CSS for an enhanced UX.",
+      "SEO optimization for website.",
+    ],
+    href: "https://ziohair.vn/",
+    logo: "",
+    image: "/assets/projects/ziohair.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "CSS",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 3,
+        name: "Typescript",
+        path: "/assets/logos/typescript.svg",
+      },
+      {
+        id: 4,
+        name: "NextJS",
+        path: "/assets/logos/next-js.svg",
+      },
+      {
+        id: 5,
+        name: "Gsap",
+        path: "/assets/logos/gsap.svg",
+      },
+      {
+        id: 6,
+        name: "Radix UI",
+        path: "/assets/logos/radixui.svg",
+      },
+      {
+        id: 6,
+        name: "Zustand",
+        path: "/assets/logos/zustand.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "On Ngoc Beu",
+    description:
+      "A lightweight C++ game engine designed for 2D and 3D game development.",
+    subDescription: [
+      "Developed UI from figma file, responsive, animation with Gsap.",
+      "Interact with the API to process data.",
+      "Designed responsive UI components with Tailwind CSS for an enhanced UX.",
+      "SEO optimization for website.",
+    ],
+    href: "https://onngocbeuvinvi.edu.vn/",
+    logo: "",
+    image: "/assets/projects/onngoc.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "CSS",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 3,
+        name: "Javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 4,
+        name: "Wordpress",
+        path: "/assets/logos/wordpress.svg",
+      },
+      {
+        id: 5,
+        name: "Gsap",
+        path: "/assets/logos/gsap.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "CRM Vilead",
+    description:
+      "Our team has designed a customer relationship management system that includes centralized management functions, security, and comprehensive customer service for all businesses.",
+    subDescription: [
+      "Developed responsive UI components based on Figma designs.",
+      "Integrated frontend with backend APIs to display and manage CRM data.",
+      "Maintained the CRM system by fixing UI bugs and improving existing features.",
+      "Implemented real-time chat for customer communication via Zalo integration.",
+      "Collaborated with backend developers and Business Analysts to implement client requirements.",
+
+    ],
+    href: "https://vilead.crmsales.vn/",
+    logo: "",
+    image: "/assets/projects/vilead.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "CSS",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 3,
+        name: "Typescript",
+        path: "/assets/logos/typescript.svg",
+      },
+      {
+        id: 4,
+        name: "NextJS",
+        path: "/assets/logos/next-js.svg",
+      },
+      {
+        id: 5,
+        name: "Firebase",
+        path: "/assets/logos/firebase.svg",
+      },
+      {
+        id: 6,
+        name: "Tailwind CSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Saigon Uniform",
+    description:
+      "An e-commerce website specializing in providing high-quality uniforms for businesses, schools, and organizations. The platform showcases product catalogs, enables online ordering, and facilitates customer inquiries for bulk purchases.",
+    subDescription: [
+      "Developed a responsive WordPress theme using HTML5, CSS3, and JavaScript.",
+      "Integrated Tailwind CSS for modern styling and UI enhancements.",
+      "Optimized SEO and page speed using Vite.js for fast builds.",
+      "Implemented custom widgets and plugin compatibility for extended functionality.",
+    ],
+    href: "https://saigonuniform.com/",
+    logo: "",
+    image: "/assets/projects/saigon.png",
+    tags: [
+      {
+        id: 1,
+        name: "WordPress",
+        path: "/assets/logos/wordpress.svg",
+      },
+      {
+        id: 2,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 4,
+        name: "JS",
+        path: "/assets/logos/javascript.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Ha Giang Local",
+    description:
+      "A fully customizable WordPress theme optimized for performance and SEO.",
+    subDescription: [
+      "Developed a responsive WordPress theme using HTML5, CSS3, and JavaScript.",
+      "Integrated Tailwind CSS for modern styling and UI enhancements.",
+      "Optimized SEO and page speed using Vite.js for fast builds.",
+      "Implemented custom widgets and plugin compatibility for extended functionality.",
+    ],
+    href: "https://hagiangtop.com/",
+    logo: "",
+    image: "/assets/projects/hagiang.png",
+    tags: [
+      {
+        id: 1,
+        name: "WordPress",
+        path: "/assets/logos/wordpress.svg",
+      },
+      {
+        id: 2,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 4,
+        name: "JS",
+        path: "/assets/logos/javascript.svg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Banana",
+    description:
+      "A fully customizable WordPress theme optimized for performance and SEO.",
+    subDescription: [
+      "Developed a responsive WordPress theme using HTML5, CSS3, and JavaScript.",
+      "Integrated Tailwind CSS for modern styling and UI enhancements.",
+      "Optimized SEO and page speed using Vite.js for fast builds.",
+      "Implemented custom widgets and plugin compatibility for extended functionality.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/banana.webp",
+    tags: [
+      {
+        id: 1,
+        name: "WordPress",
+        path: "/assets/logos/wordpress.svg",
+      },
+      {
+        id: 2,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 4,
+        name: "JS",
+        path: "/assets/logos/javascript.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "Github",
+    href: "https://github.com/NgocMinh10122001",
+    icon: "/assets/socials/github.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/ng%E1%BB%8Dc-minh-ph%E1%BA%A1m-854312374/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/minhpham541/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Intern Frontend Developer",
+    job: "Okhub Agency",
+    date: "2024-2025",
+    contents: [
+      "Trained in programming skills using technologies such as React JS, Next JS, Gsap, and Zustand.",
+      "Perform CRUD exercises with RESTful API.",
+      "Learn and adhere to project rules and standards within the company.",
+    ],
+  },
+  {
+    title: "Fresher Frontend Developer",
+    job: "Okhub Agency",
+    date: "2025",
+    contents: [
+      "Developed and delivered multiple dynamic landing pages across tourism, education.",
+      "Implemented responsive UI based on Figma designs, ensuring cross-device and cross-browser compatibility.",
+      "✅ Built reusable components and standardized UI structure to accelerate development across multiple projects.",
+      "✅ Optimized page performance through code splitting, lazy loading, and asset optimization.",
+      "✅ Improved SEO by applying semantic HTML, meta tags, and structured content.",
+      "✅ Collaborated closely with designers and backend teams to deliver user-friendly and visually consistent interfaces.",
+      "✅ Worked on multiple projects simultaneously, ensuring consistent UI quality and timely delivery.",
+    ],
+  },
+  {
+    title: "Fresher Frontend Developer",
+    job: "Vitech Group",
+    date: "2025-Present",
+    contents: [
+      "Collaborated in a team to develop frontend for a CRM web application focused on user and customer management.",
+      "Designed and implemented core features for user and customer management, improving data handling and workflow efficiency.",
+      "Built scalable and maintainable frontend architecture using Next.js, TypeScript, and modern state management.",
+      "Developed dynamic data tables, filtering, and form handling for managing large datasets.",
+      "Integrated GraphQL APIs and managed asynchronous data fetching for dynamic UI.",
+      "Optimized performance and improved user experience for complex data-driven interfaces.",
+      "Deployed and maintained features across multiple environments based on project requirements.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "This feature is currently being finalized. It will be released soon. Thank you.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "This feature is currently being finalized. It will be released soon. Thank you.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "This feature is currently being finalized. It will be released soon. Thank you.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This feature is currently being finalized. It will be released soon. Thank you.",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "This feature is currently being finalized. It will be released soon. Thank you.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This feature is currently being finalized. It will be released soon. Thank you.",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "This feature is currently being finalized. It will be released soon. Thank you.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "This feature is currently being finalized. It will be released soon. Thank you.",
+    img: "https://robohash.org/eve",
+  },
+];
